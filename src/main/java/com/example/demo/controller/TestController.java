fd370b51f923639cf6a46ae0b1987e36f1240828
slalom0914 <slalom0914@gmail.com>
2025-07-25 07:40:45 +0900

    20250724

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,23 @@
 package com.example.demo.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +30,100 @@ import com.example.demo.service.TestService;
 import lombok.extern.log4j.Log4j2;
 import com.example.demo.vo.TestVO;
 import com.google.gson.Gson;
+import org.springframework.web.multipart.MultipartFile;
 
 @Log4j2
 @RestController
 @RequestMapping("/test/*")
 public class TestController {
+  @Value("${uploadPath}")
+  private String uploadPath;
   @Autowired
   private TestService testService = null;
+
+  //이미지 업로드 하기
+  @PostMapping("imageUpload")
+  public String imageUpload(@RequestParam(value="image") MultipartFile image) {
+    log.info("imageUpload");
+    log.info("image : " + image);
+    //리액트에서 요청된 이미지 파일을 톰캣 서버에 pds폴더에 업로드 처리함
+    String filename = testService.imageUpload(image);
+    return filename;
+  }
+  //이미지 읽어오기 - Quill Editor 이미지 미리보기용
+  // http://localhost:8000/test/imageGet?imageName=man.png
+  @GetMapping("imageGet")
+  public String imageGet(HttpServletRequest req, HttpServletResponse res) {
+    log.info("imageGet");
+    String b_file = req.getParameter("imageName");
+    log.info("b_file : " + b_file);
+    String filePath = "D:\\dev_lab\\07.myBatis\\dev-mybatis\\src\\main\\webapp\\pds";
+    String fname = b_file;
+    File file = new File(filePath, b_file.trim());
+    String mimeType = req.getServletContext().getMimeType(file.toString());
+    if (mimeType == null) {
+      res.setContentType("application/octet-stream");
+    }
+    String downName = null;
+    FileInputStream fis = null;
+    ServletOutputStream sos = null;
+    try{
+      if(req.getHeader("User-Agent").indexOf("MSIE") == -1){
+        downName = new String(b_file.getBytes("UTF-8"),"8859_1");
+      }else{
+        downName = new String(b_file.getBytes("EUC-KR"),"8859_1");
+      }
+      res.setHeader("Content-Disposition", "attachment; filename=" + downName);
+      fis = new FileInputStream(file);
+      sos = res.getOutputStream();
+      byte[] b = new byte[1024*10];
+      int data = 0;
+      while((data = (fis.read(b, 0, b.length))) != -1){
+        sos.write(b,0,data);
+      }
+      sos.flush();
+    }catch (Exception e){
+      e.printStackTrace();
+    }finally {
+      try {
+        if(sos != null) sos.close();
+        if(fis != null) fis.close();
+      }catch (Exception e){
+        e.printStackTrace();
+      }
+    }
+    return null;
+  }
+  //이미지 다운로드
+  //imageName키값은 스프링에서 request.getParameter가 아니어도 사용자가 입력한 값을 읽어올 수 있다.
+  //emp.ephoto값은 오라클 서버에서 select한 결과값이다.
+  //http://localhost:8000/emp/imageDownload?imageName=emp.ephoto
+  @GetMapping("imageDownload")
+  public ResponseEntity<Resource> imageDownload(@RequestParam(value="imageName") String imageName) {
+    log.info("imageDownload");
+    try {
+      File file = new File(uploadPath, URLDecoder.decode(imageName, "UTF-8"));
+      HttpHeaders header = new HttpHeaders();
+      header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + imageName);
+      header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+      header.add("Pragma", "no-cache");
+      header.add("Expires", "0");
+
+      Path path = Paths.get(file.getAbsolutePath());
+      ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+      return ResponseEntity.ok()
+              .headers(header)
+              .contentLength(file.length())
+              .contentType(MediaType.parseMediaType("application/octet-stream"))
+              .body(resource);
+
+    }catch (Exception e){
+      e.printStackTrace();
+      return null;
+    }
+  }//end of imageDownload
+
+
   //SELECT t_no, t_title, t_content FROM test
   @GetMapping("testList") 
   public String testList(){
--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -1,19 +1,27 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.TestDao;
 import com.example.demo.vo.TestVO;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.multipart.MultipartFile;
 
 @Log4j2
 @Service
 public class TestService {
+  //resources>application.yml
+  @Value("${uploadPath}")
+  private String uploadPath;
   @Autowired
   private TestDao testDao = null;
 
@@ -44,4 +52,39 @@ public class TestService {
     result = testDao.testDelete(t_no);
     return result;
   }
+
+  public String imageUpload(MultipartFile image) {
+    Map<String,Object> map = new HashMap<>();
+    //이미지파일을 톰캣 서버가 바라보는 프로젝트 폴더 pds에 업로드 한다.
+    String filename = null;
+    String fullPath = null;
+    //이미지 파일이 존재하면....
+    //만일 같은 이름의 파일이 존재하면 안되니까.... 예방코드 작성
+    if(image!=null && !image.isEmpty()){
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+      Calendar time = Calendar.getInstance();
+      filename = sdf.format(time.getTime())+"-"+image.getOriginalFilename().replaceAll(" ","-");
+      fullPath = uploadPath+filename;
+      try {
+        //File객체는 파일명을 객체로 만들어주는 클래스 이다.
+        File f = new File(fullPath);
+        //위에서 File객체는 파일명만 생성할 뿐 내용까지 포함되는 건 아니다.
+        byte[] bytes = image.getBytes();
+        //OutputStream을 생성하여 파일객체에 읽어들인 내용들을 쓴다.
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(f));
+        out.write(bytes);
+        //입출력 관련 클래스는 사용 후 반드시 닫아준다.
+        out.close();
+        //파일 처리하는 경우 - 추가 파일 정보가 필요할 때
+        //파일 크기
+        double size = Math.floor(filename.length()/(1024.0*1024.0)*10/10);
+        log.info(size);
+        map.put("filename",filename);
+        map.put("filesize",size);
+      }catch (Exception e){
+        e.printStackTrace();
+      }
+    }//end of if
+    return filename;
+  }//imageUpload
 }
